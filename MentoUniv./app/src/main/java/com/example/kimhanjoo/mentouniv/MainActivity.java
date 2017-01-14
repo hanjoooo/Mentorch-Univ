c157c26b1119398bd3bbe6b235a3d844a7c98b37
kimhanjoo <khanjoo113@naver.com>
2017-01-14 13:11:47 +0900

    Add listview.

--- MentoUniv./app/src/main/java/com/example/kimhanjoo/mentouniv/MainActivity.java
+++ MentoUniv./app/src/main/java/com/example/kimhanjoo/mentouniv/MainActivity.java
@@ -3,17 +3,88 @@ package com.example.kimhanjoo.mentouniv;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.Menu;
+import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.RelativeLayout;
+
+import java.util.ArrayList;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
     private RelativeLayout mlayout;
+    ListView list;
+    ListViewAdapter adapter;
+    EditText editsearch;
+    String[] rank;
+    String[] country;
+    String[] population;
+    ArrayList<WorldPopulation> arraylist = new ArrayList<WorldPopulation>();
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         mlayout = (RelativeLayout) findViewById(R.id.activity_main);
         mlayout.setBackgroundColor(Color.rgb(148,210,238));
+        // Generate sample data
+        rank = new String[] { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10" };
+
+        country = new String[] { "미분적분학", "물리및실험2", "전자기학",
+                "미분방정식", "회로이", "미분방정식", "Nigeria", "물리전자공학",
+                "공학수", "미분적분학" };
+
+        population = new String[] { "어려움", "어려움",
+                "쉬움", "보통", "어려움", "보통",
+                "보통", "쉬움", "어려움", "보통" };
+
+        // Locate the ListView in listview_main.xml
+        list = (ListView) findViewById(R.id.listview);
+
+        for (int i = 0; i < rank.length; i++)
+        {
+            WorldPopulation wp = new WorldPopulation(rank[i], country[i],
+                    population[i]);
+            // Binds all strings into an array
+            arraylist.add(wp);
+        }
+
+        // Pass results to ListViewAdapter Class
+        adapter = new ListViewAdapter(this, arraylist);
+
+        // Binds the Adapter to the ListView
+        list.setAdapter(adapter);
+
+        // Locate the EditText in listview_main.xml
+        editsearch = (EditText) findViewById(R.id.search);
+
+        // Capture Text in EditText
+        editsearch.addTextChangedListener(new TextWatcher() {
+
+            @Override
+            public void afterTextChanged(Editable arg0) {
+                // TODO Auto-generated method stub
+                String text = editsearch.getText().toString().toLowerCase(Locale.getDefault());
+                adapter.filter(text);
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence arg0, int arg1,
+                                          int arg2, int arg3) {
+                // TODO Auto-generated method stub
+            }
+
+            @Override
+            public void onTextChanged(CharSequence arg0, int arg1, int arg2,
+                                      int arg3) {
+                // TODO Auto-generated method stub
+            }
+        });
     }
+
+    // Not using options menu in this tutorial
 }
